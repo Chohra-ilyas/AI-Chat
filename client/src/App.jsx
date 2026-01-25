034ce15d0cbd3d4357888e9c5a7a9defbc1e61d4
chohra ilyas <chohrailyas16@gmail.com>
2026-01-25 15:50:00 +0100

    feat: implement responsive SideBar with menu toggle and user account display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,20 @@ import SideBar from "./components/SideBar";
 import Community from "./pages/Community";
 import Credits from "./pages/Credits";
 import { Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import { assets } from "./assets/assets";
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <>
+    {!isMenuOpen &&  <img src={assets.menu_icon} alt="Close Menu"
+    className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+    onClick={() => setIsMenuOpen(true)}
+    />}
       <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
-          <SideBar />
+          <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
           <Routes>
             <Route path="/" element={<ChatBox />} />
             <Route path="/credits" element={<Credits />} />
--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -2,14 +2,15 @@ import { useState } from "react";
 import { assets } from "../assets/assets";
 import { useAppContext } from "../context/AppContext";
 import moment from "moment";
-const SideBar = () => {
+const SideBar = ({ isMenuOpen, setIsMenuOpen }) => {
   const { chats, setSelectedChat, theme, setTheme, user, navigate } =
     useAppContext();
   const [search, setSearch] = useState("");
   return (
     <div
-      className="flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242142]/30 to-[#000000]
-    border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1 "
+      className={`flex flex-col h-screen min-w-72 p-5 dark:bg-gradient-to-b from-[#242142]/30 to-[#000000]
+    border-r border-[#80609F]/30 backdrop-blur-3xl transition-all duration-500 max-md:absolute left-0 z-1
+    ${!isMenuOpen && "max-md:-translate-x-full"} md:translate-x-0`}
     >
       <img
         className="w-full max-w-48"
@@ -52,6 +53,11 @@ const SideBar = () => {
           )
           .map((chat) => (
             <div
+              onClick={() => {
+                navigate("/");
+                setSelectedChat(chat);
+                setIsMenuOpen(false);
+              }}
               key={chat._id}
               className="p-2 px-4 dark:bg-[#57317C]/10 border border-gray-300
           dark:border-[#80609F]/15 rounded-md cursor-pointer flex justify-between group"
@@ -76,7 +82,10 @@ const SideBar = () => {
       </div>
       {/* Community Images */}
       <div
-        onClick={() => navigate("/community")}
+        onClick={() => {
+          navigate("/community");
+          setIsMenuOpen(false);
+        }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300
       dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -92,7 +101,10 @@ const SideBar = () => {
 
       {/* Credit Purchases Option */}
       <div
-        onClick={() => navigate("/credits")}
+        onClick={() => {
+          navigate("/credits");
+          setIsMenuOpen(false);
+        }}
         className="flex items-center gap-2 p-3 mt-4 border border-gray-300
       dark:border-white/15 rounded-md cursor-pointer hover:scale-103 transition-all"
       >
@@ -139,6 +151,32 @@ const SideBar = () => {
           ></span>
         </label>
       </div>
+
+      {/* User Account */}
+      <div
+        className="flex items-center gap-3 p-3 mt-4 border border-gray-300
+      dark:border-white/15 rounded-md cursor-pointer group"
+      >
+        <img src={assets.user_icon} alt="User" className="w-7 rounded-full" />
+        <p className="flex-1 text-sm dark:text-primary truncate">
+          {user ? user?.name : "Login to your account"}
+        </p>
+        {user && (
+          <img
+            src={assets.logout_icon}
+            className="w-4 cursor-pointer hidden group-hover:block not-dark:invert"
+            alt="Logout"
+          />
+        )}
+      </div>
+
+      <img
+        src={assets.close_icon}
+        className="absolute top-3 right-3 w-5 h-5
+        cursor-pointer md:hidden not-dark:invert"
+        alt=""
+        onClick={() => setIsMenuOpen(false)}
+      />
     </div>
   );
 };
