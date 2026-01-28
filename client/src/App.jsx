60352ee50d83f3cca94b774f2629aac1a424b272
chohra ilyas <chohrailyas16@gmail.com>
2026-01-28 10:00:52 +0100

    feat: implement image fetching and loading state in Community component; enhance layout for displaying images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,24 @@ import Credits from "./pages/Credits";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { use, useState } from "react";
 import { assets } from "./assets/assets";
-import './assets/prism.css';
+import "./assets/prism.css";
 import Loading from "./pages/Loading";
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const {pathname} = useLocation();
+  const { pathname } = useLocation();
 
   if (pathname === "/loading") return <Loading />;
 
   return (
     <>
-    {!isMenuOpen &&  <img src={assets.menu_icon} alt="Close Menu"
-    className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
-    onClick={() => setIsMenuOpen(true)}
-    />}
+      {!isMenuOpen && (
+        <img
+          src={assets.menu_icon}
+          alt="Close Menu"
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+          onClick={() => setIsMenuOpen(true)}
+        />
+      )}
       <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
         <div className="flex h-screen w-screen">
           <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
