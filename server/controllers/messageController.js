295a325392e0a72e0fe7ca0f9af5803b2fe6b0b7
chohra ilyas <chohrailyas16@gmail.com>
2026-02-01 11:10:23 +0100

    feat: implement image message functionality and integrate ImageKit for image uploads

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,8 @@
 import ai from "../config/openAI.js";
 import Chat from "../models/Chat.js";
 import User from "../models/User.js";
+import axios from "axios";
+import imagekit from "../config/imageKit.js";
 
 // Text-based AI Chat Message Controller
 export const textMessageController = async (req, res) => {
@@ -22,16 +24,74 @@ export const textMessageController = async (req, res) => {
     });
 
     const reply = extractMessage(response);
-    
+
     chat.messages.push({ role: "assistant", content: reply, isImage: false });
     await chat.save();
     await User.updateOne({ _id: userId }, { $inc: { credits: -1 } });
     res.status(200).json({ message: reply, success: true });
-
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message, success: false });
   }
+};
+
+export const imageMessageController = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    if (req.user.credits < 2) {
+      return res.status(403).json({
+        message: "you need more credits to use this feature",
+        success: false,
+      });
+    }
+    const { chatId, prompt, isPublished } = req.body;
+
+    const chat = await Chat.findOne({ _id: chatId, userId });
+    if (!chat) {
+      return res
+        .status(404)
+        .json({ message: "Chat not found", success: false });
+    }
+    chat.messages.push({ role: "user", content: prompt, isImage: false });
+
+    const endcodedPrompt = encodeURIComponent(prompt);
+
+    const generateImageUrl = `${process.env.URL_ENDPOINT}/
+    ik-genimg-prompt-${endcodedPrompt}/AI-Chat/${Date.now()}.png?tr=w-800,h-800`;
+
+    const AIImageResponse = await axios.get(generateImageUrl, {
+      responseType: "arraybuffer",
+    });
+
+    const base64Image = `data:image/png;base64,${Buffer.from(
+      AIImageResponse.data,
+      "binary",
+    ).toString("base64")}`;
+
+    const uploadResponse = await imagekit.upload({
+      file: base64Image,
+      fileName: `${Date.now()}.png`,
+      folder: "AI-Chat",
+    });
+
+    const reply = {
+      role: "assistant",
+      content: uploadResponse.url,
+      isPublished,
+      isImage: true,
+    };
+    
+    chat.messages.push(reply);
+    await chat.save();
+    await User.updateOne({ _id: userId }, { $inc: { credits: -2 } });
+    res
+      .status(200)
+      .json({
+        message: "Image generated successfully",
+        data: reply,
+        success: true,
+      });
+  } catch (error) {}
 };
 
 const extractMessage = (response) => {
--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { textMessageController } from "../controllers/messageController.js";
+import {
+  imageMessageController,
+  textMessageController,
+} from "../controllers/messageController.js";
 import { authenticateUser } from "../middlewares/auth.js";
 
 const messageRoute = express.Router();
 
 messageRoute.use(authenticateUser);
 messageRoute.post("/text", textMessageController);
+messageRoute.post("/image", imageMessageController);
 
 export default messageRoute;
